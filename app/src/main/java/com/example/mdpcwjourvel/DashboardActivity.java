84ca5a90f6a42e181256523a4a739d0bed120c99
JiaHui9012 <goh-jia-hui@hotmail.com>
2021-12-13 13:14:24 +0800

    updated

--- app/src/main/java/com/example/mdpcwjourvel/DashboardActivity.java
+++ app/src/main/java/com/example/mdpcwjourvel/DashboardActivity.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.FragmentTransaction;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -49,6 +50,8 @@ public class DashboardActivity extends AppCompatActivity {
         user = firebaseAuth.getCurrentUser();
         firebaseDatabase = FirebaseDatabase.getInstance();
         databaseReference = firebaseDatabase.getReference("Users");
+
+        checkUserStatus();
 
         //init views
         //profileTV = findViewById(R.id.profileTextView);
